9263556a89382241d0f545e62d5186b2a1bb0fba
ryanmaksymic <ryanmaksymic@gmail.com>
2018-02-28 10:43:49 -0500

    Created Photo and Quote classes

---
+++ QuotePro/Photo.swift
@@ -0,0 +1,19 @@
+//
+//  Photo.swift
+//  QuotePro
+//
+//  Created by Ryan Maksymic on 2018-02-28.
+//  Copyright © 2018 Ryan Maksymic. All rights reserved.
+//
+
+import UIKit
+
+class Photo
+{
+  var image: UIImage
+  
+  init(image: UIImage)
+  {
+    self.image = image
+  }
+}
---
+++ QuotePro/Quote.swift
@@ -0,0 +1,22 @@
+//
+//  Quote.swift
+//  QuotePro
+//
+//  Created by Ryan Maksymic on 2018-02-28.
+//  Copyright © 2018 Ryan Maksymic. All rights reserved.
+//
+
+import Foundation
+
+class Quote
+{
+  var quote: String
+  var source: String
+  var photo: Photo?
+  
+  init(quote: String, source: String)
+  {
+    self.quote = quote
+    self.source = source
+  }
+}
