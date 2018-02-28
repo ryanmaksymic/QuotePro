182890ea4a39d20cc33b8f3ec6a9998498272aa7
ryanmaksymic <ryanmaksymic@gmail.com>
2018-02-28 10:37:40 -0500

    Initial Commit

---
+++ QuotePro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuotePro
+//
+//  Created by Ryan Maksymic on 2018-02-28.
+//  Copyright © 2018 Ryan Maksymic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
