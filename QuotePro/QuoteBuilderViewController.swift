30d254ee6266e211e2d56e987689f24ade958504
ryanmaksymic <ryanmaksymic@gmail.com>
2018-02-28 11:11:36 -0500

    Set up outlets and actions for QuoteBuilder

--- QuotePro/QuoteBuilderViewController.swift
+++ QuotePro/QuoteBuilderViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class QuoteBuilderViewController: UIViewController {
-
-  override func viewDidLoad() {
+class QuoteBuilderViewController: UIViewController
+{
+  // MARK: - Properties
+  
+  @IBOutlet weak var quoteLabel: UILabel!
+  @IBOutlet weak var sourceLabel: UILabel!
+  @IBOutlet weak var quoteImageView: UIImageView!
+  
+  
+  override func viewDidLoad()
+  {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  // MARK: - Actions
+  @IBAction func randomQuote(_ sender: UIButton)
+  {
+    
   }
-
-
+  
+  @IBAction func randomPhoto(_ sender: UIButton)
+  {
+    
+  }
 }
 
